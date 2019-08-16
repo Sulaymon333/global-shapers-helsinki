cc55821c1c933dd9330814617ed1911676b8fedd
MirelaViit <mirela2608@yahoo.com>
2019-08-16 17:23:01 +0300

    imported icons and social media extrnal links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Link } from 'gatsby'
+
+/*Icons
+
+import logo from '../assets/gsh-logo.png'
+import facebook from '../assets/social-icons/facebook.svg'
+import instagram from '../assets/social-icons/instagram.svg'
+import linkedIn from '../assets/social-icons/linkedin.svg'
+import twitter from '../assets/social-icons/medium.svg' 
+import youTube from '../assets/social-icons/youTube' // IMPORTANT! I dont have icon still
+*/
+
+import footerStyles from './footer.scss'
 
 const Footer = () => {
   return <div>This is the Footer component</div>
 }
 
 export default Footer
+
+/*social media adreses
+
+<a href="https://www.facebook.com/ShapersHel/">facebook</a>  
+<a href="https://twitter.com/globalshapers">twitter</a>     
+<a href="https://www.instagram.com/shapershel/">instagram</a>  
+<a href="https://www.linkedin.com/company/shapershel/">linkedIn</a>   
+<a href="https://www.youtube.com/globalshapers">youTube</a>    
+*/
