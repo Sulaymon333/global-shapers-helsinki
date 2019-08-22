d7832792c599bf4f00416691cb7ef8e84f10021e
kunjan <ghimirekunji@gmail.com>
2019-08-22 15:32:52 +0300

    made member branch

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,12 @@
-
 module.exports = {
-  plugins: [`gatsby-plugin-sass`]
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    `gatsby-plugin-sass`,
+  ],
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,18 @@
-import React from "react"
-import Header from "./Header"
- import '../styles/index.scss'
+import React from 'react'
+import Header from './Header'
+import '../styles/index.scss'
 import layoutstyles from './layout.module.scss'
-import Hero from'./Hero'
+import Hero from './Hero'
+import Members from './Members'
+import Community from './Community'
 
 const Layout = () => {
   return (
-    <div className = {layoutstyles.container}>
+    <div className={layoutstyles.container}>
       <Header />
-      <Hero/>
-
-      
+      <Hero />
+      <Community />
+      <Members />
     </div>
   )
 }
