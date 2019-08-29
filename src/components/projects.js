fa7974e023a1a4a6dff58e106c4899a641b2ee81
Sulaymon333 <tjsulaymon@gmail.com>
2019-08-29 15:12:47 +0300

    project section completed, footer styles fixed for mobile view

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,46 +3,58 @@ import { graphql, useStaticQuery, Link } from 'gatsby'
 
 import projectsStyles from './projects.module.scss'
 
-import projectImage from '../assets/project-images/project-1/project-1.jpg'
 import projectLocation from '../assets/icons/Location.svg'
 
 const Projects = () => {
   const data = useStaticQuery(graphql`
-    query {
-      allContentfulMembers {
+    {
+      allContentfulProjects {
         edges {
           node {
-            firstname
-            lastname
-            title
-            linkedInUrl
-            profilePicture {
+            id
+            coverImage {
               file {
                 url
               }
             }
+            title
+            description
+            location
           }
         }
       }
     }
   `)
+
+  const { edges } = data.allContentfulProjects
+
   return (
     <section className={projectsStyles.projectsSection}>
       <div className={projectsStyles.container}>
         <h1 className={projectsStyles.title}>GSH Projects</h1>
-        <div className={projectsStyles.projectBox}>
-          <img src={projectImage} alt="project image" />
-          <p className={projectsStyles.projectTitle}>Red Cross Project</p>
-          <p className={projectsStyles.projectDescription}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            fugiat dolor cum totam veritatis laborum.
-          </p>
-          <div className={projectsStyles.projectLocation}>
-            <img src={projectLocation} alt="location icon" />
-            <span className={projectsStyles.location}>
-              Global Shapers Helsinki
-            </span>
-          </div>
+        <div className={projectsStyles.projectBoxes}>
+          {edges.map(edge => {
+            return (
+              <div className={projectsStyles.projectBox} key={edge.node.id}>
+                <div className={projectsStyles.projectCoverImage}>
+                  <img
+                    src={edge.node.coverImage.file.url}
+                    alt="project image"
+                  />
+                </div>
+                <p className={projectsStyles.projectTitle}>{edge.node.title}</p>
+                <p className={projectsStyles.projectDescription}>
+                  {edge.node.description}
+                </p>
+                <div className={projectsStyles.projectLocation}>
+                  <img src={projectLocation} alt="location icon" />
+                  <span className={projectsStyles.location}>
+                    {edge.node.location}
+                  </span>
+                </div>
+              </div>
+            )
+          })}
         </div>
       </div>
     </section>
